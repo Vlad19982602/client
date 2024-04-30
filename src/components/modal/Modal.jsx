b5369fa68aa45a2c1d58ba77fe0de0d8750993bc
Vlad Fedorov <79852880+Vlad19982602@users.noreply.github.com>
2024-04-30 14:00:36 +0500

    npm update

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -3,12 +3,17 @@ import './modal.css'
 
 import CloseButton from 'react-bootstrap/CloseButton';
 
+
 const Modal = ({active, setActive, children}) => {
+
+    function handleClick(close) {
+        alert('You clicked me!');
+    }
 
     return(
         <>
-            <div className='modal-window' onClick={() => setActive=(false)}>
-                <CloseButton id='closeBtn' className='modal__btn-close'/>
+            <div id='modal' className='modal-window' onClick={() => setActive=(false)}>
+                <CloseButton onClick={handleClick} className='modal__btn-close'/>
                 <div className='modal__wrapper' onClick={e => e.stopPropagation()}>
                     <div className='modal__image-wrapper'>
                         <div className='modal__image-pic'></div>
@@ -21,8 +26,8 @@ const Modal = ({active, setActive, children}) => {
                         </div>
                     </div>
                     <div className='modal__text-phone bold'>+7 (922) 630-21-03</div>
-                    {children}
                 </div>
+                {children}
             </div>
         </>
     )

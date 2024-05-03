caf632040a31928ca6ea86270e84c1daa651e707
Vlad Fedorov <79852880+Vlad19982602@users.noreply.github.com>
2024-05-03 17:42:34 +0500

    writing a section with services, layou [1]

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Decoration from './components/decoration/Decoration';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 import Modal from './components/modal/Modal';
+import Slider from './components/slider/Slider';
+import SliderContent from './components/slider/SliderContent';
 import SubTitle from './components/subtitlePage/SubTitle';
 import GoTop from './components/topButton/GoTop';
 import { useEffect, useState, useRef } from 'react';
@@ -12,7 +14,15 @@ import { useEffect, useState, useRef } from 'react';
 
 function App() {
 
-  
+  const [open, setOpen] = useState(true);
+
+  const handleOpenModal = () => {
+    setTimeout(() => {
+      setOpen(false);
+    }, 60000);
+    setOpen(true);
+  };
+
   const [scrollPosition, setSrollPosition] = useState(0);
   const [showGoTop, setshowGoTop] = useState("goTopHidden");
   const refScrollUp = useRef();
@@ -40,14 +50,18 @@ function App() {
 
   return (
     <div className="App">
+
       <div ref={refScrollUp}> </div>
+      <div timer={handleOpenModal}/>
       <GoTop showGoTop={showGoTop} scrollUp={handleScrollUp} />
-      <Modal />
+      <Modal open={open} setOpen={setOpen} />
       <Header/>
       <SubTitle/>
       <Main/>
       <Calculator/>
-      <Decoration/>
+      <Decoration title={"РАБОТАЕМ С ОБЪЕКТАМИ ЛЮБОЙ СЛОЖНОСТИ"}/>
+      <Slider/>
+      <SliderContent />
     </div>
   );
 }

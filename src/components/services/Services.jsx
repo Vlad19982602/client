e726efba46d68552a9891340e78de602497fa99b
Vlad Fedorov <79852880+Vlad19982602@users.noreply.github.com>
2024-05-07 16:50:39 +0500

    added section with services organization

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,82 @@
+import './services.css'
+
+const Services = () => {
+    return ( 
+        <div className="services">
+            <div className="container">
+                <div className='row'>
+                    <div className='col-lg-8 col-lg-offset-1 col-md-10 col-md-offset-1 mt-100'>
+                        <ul className='services__container t-container'>
+                            <li className='t-list__item d-flex'>
+                                <div className="services__item d-flex ">
+                                    <div className="t-cell t-valign_top">
+                                        <div className="services__imgwrapper" style={{ width: 50 }}>
+                                            <svg role="presentation" className="services__checkmark" fill="#76b82a" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 31.8 31.8">
+                                                <path d="M.9 14.5C5.2 19.4 9.8 24 14.1 29c.4.5 1.3.3 1.6-.2 5.1-8.4 10.1-16.8 15-25.4.7-1.2-.5-2.1-1.3-.9C19.3 18 19 18.4 13.9 26.7c.5-.1 1.1-.1 1.6-.2-4.4-4.8-8.9-8.6-13.9-12.8-.4-.4-1.1.3-.7.8z"></path>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <div className="services__textwrapper t-cell t-valign_top">
+                                        <div className="services__descr t-descr t-descr_sm" field="li_descr__1528780144966">
+                                            <div>Отвечаем на все вопросы, выезжаем на объект для сбора данных, подготавливаем техническое решение, в течении всего срока эксплутации совершаем информационную поддержку - <strong>БЕСПЛАТНО.</strong></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className="t-list__item d-flex" >
+                                <div className="services__item d-flex ">
+                                    <div className="t-cell t-valign_top">
+                                        <div className="services__imgwrapper" style={{ width: 50 }}>
+                                            <svg role="presentation" className="services__checkmark" fill="#76b82a" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 31.8 31.8">
+                                                <path d="M.9 14.5C5.2 19.4 9.8 24 14.1 29c.4.5 1.3.3 1.6-.2 5.1-8.4 10.1-16.8 15-25.4.7-1.2-.5-2.1-1.3-.9C19.3 18 19 18.4 13.9 26.7c.5-.1 1.1-.1 1.6-.2-4.4-4.8-8.9-8.6-13.9-12.8-.4-.4-1.1.3-.7.8z"></path>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <div className="services__textwrapper t-cell t-valign_top" >
+                                        <div className="services__descr t-descr t-descr_sm" field="li_descr__1476889075209">
+                                            <div>Благодаря слаженной команде профессионалов (как монтажников, так и инженеров), собственному производству воздуховодов, ведём адекватную финансовую политику, что существенно <strong>сократит Ваши расходы</strong>.</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className="t-list__item d-flex">
+                                <div className="services__item d-flex ">
+                                    <div className="t-cell t-valign_top">
+                                        <div className="services__imgwrapper" style={{ width: 50 }}>
+                                            <svg role="presentation" className="services__checkmark" fill="#76b82a" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 31.8 31.8">
+                                                <path d="M.9 14.5C5.2 19.4 9.8 24 14.1 29c.4.5 1.3.3 1.6-.2 5.1-8.4 10.1-16.8 15-25.4.7-1.2-.5-2.1-1.3-.9C19.3 18 19 18.4 13.9 26.7c.5-.1 1.1-.1 1.6-.2-4.4-4.8-8.9-8.6-13.9-12.8-.4-.4-1.1.3-.7.8z"></path>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <div className="services__textwrapper t-cell t-valign_top">
+                                        <div className="services__descr t-descr t-descr_sm" field="li_descr__1476889079427">
+                                            <div><strong>Быстрое выполнение работ точно в срок</strong>. Перед заключением договора мы согласовываем и фиксируем график наших работ и в дальнейшем строго его придерживаемся, укладываясь точно в срок.</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className="t-list__item d-flex">
+                                <div className="services__item d-flex ">
+                                    <div className="t-cell t-valign_top">
+                                        <div className="services__imgwrapper" style={{ width: 50 }}>
+                                            <svg role="presentation" className="services__checkmark" fill="#76b82a" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 31.8 31.8">
+                                                <path d="M.9 14.5C5.2 19.4 9.8 24 14.1 29c.4.5 1.3.3 1.6-.2 5.1-8.4 10.1-16.8 15-25.4.7-1.2-.5-2.1-1.3-.9C19.3 18 19 18.4 13.9 26.7c.5-.1 1.1-.1 1.6-.2-4.4-4.8-8.9-8.6-13.9-12.8-.4-.4-1.1.3-.7.8z"></path>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <div className="services__textwrapper t-cell t-valign_top">
+                                        <div className="services__descr t-descr t-descr_sm" field="li_descr__1529379746558">
+                                            <div>Работаем с надежным оборудованием различного класса стоимости, на все оборудование и монтажные работы даём <strong>гарантию</strong> до трёх лет, в течение 1 года после монтажа бесплатно проводим техническое обслуживание системы.</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Services
---
+++ src/components/services/ServicesMore.jsx
@@ -0,0 +1,171 @@
+import './services.css'
+
+const ServicesMore = () => {
+    return (
+    <div className='services__more'>
+        <div className='container'>
+            <div className='row'>
+                <div className="col-lg-8 col-lg-offset-1 col-md-10 col-md-offset-1 mt-100">
+                    <ul className="services__container t-container">
+                        <li
+                            className="services__col services__col_first t-list__item"
+                            style={{ marginTop: 30, transitionDelay: "0s" }}
+                        >
+                            <div className="services__item d-flex ">
+                                <div className="t-cell t-valign_top">
+                                    <div className="services__imgwrapper" style={{ width: 50 }}>
+                                    <img
+                                        className="services__img t-img"
+                                        src="https://static.tildacdn.com/lib/tildaicon/62303662-3733-4538-b136-313334653730/Tilda_Icons_33_interior_design.svg"
+                                        alt=""
+                                    />
+                                    </div>
+                                </div>
+                                <div className="services__textwrapper t-cell t-valign_top">
+                                    <div
+                                    className="services__name t-name t-name_lg"
+                                    style={{ fontSize: 20 }}
+                                    >
+                                    <div >
+                                        <strong> Проектирование и расчет</strong>
+                                    </div>
+                                    </div>
+                                    <div
+                                    className="services__descr t-descr t-descr_xs"
+                                    style={{ fontSize: 20 }}
+                                    >
+                                    <div style={{ fontSize: 16, fontFamily: '"Open Sans"' }}>
+                                        <span style={{ fontSize: 20 }}>
+                                        Многолетний опыт работы дает возможность браться за проекты
+                                        любой сложности.
+                                        </span>
+                                        <br />
+                                        <br />
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li
+                            className="services__col"
+                            style={{ marginTop: 30, transitionDelay: "0.16s" }}
+                        >
+                            <div className="services__item d-flex ">
+                                <div className="t-cell t-valign_top">
+                                    <div className="services__imgwrapper" style={{ width: 50 }}>
+                                    <img
+                                        className="services__img t-img"
+                                        src="https://static.tildacdn.com/lib/tildaicon/65633137-3839-4862-a661-663538623232/Tilda_Icons_32_profession_builder.svg"
+                                        alt=""
+                                    />
+                                    </div>
+                                </div>
+                                <div className="services__textwrapper t-cell t-valign_top">
+                                    <div
+                                    className="services__name t-name t-name_lg"
+                                    style={{ fontSize: 20 }}
+                                    >
+                                    <div style={{ fontSize: 20, fontFamily: '"Open Sans"' }}>
+                                        <strong> Монтаж</strong>
+                                    </div>
+                                    </div>
+                                    <div
+                                    className="services__descr t-descr t-descr_xs"
+                                    style={{ fontSize: 20 }}
+                                    >
+                                    <div style={{ fontSize: 16, fontFamily: '"Open Sans"' }}>
+                                        <span style={{ fontSize: 20 }}>
+                                        Постоянный штат монтажников позволяет нам выстраивать удобный
+                                        для Вас график работ, при необходимости готовы работать в
+                                        выходные и праздничные дни.
+                                        </span>
+                                        <br />
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li
+                            className="services__col t-list__item"
+                            style={{ marginTop: 30, transitionDelay: "0.32s" }}
+                        >
+                            <div className="services__item d-flex ">
+                                <div className="t-cell t-valign_top">
+                                    <div className="services__imgwrapper" style={{ width: 50 }}>
+                                    <img
+                                        className="services__img t-img"
+                                        src="https://static.tildacdn.com/lib/tildaicon/39373461-6562-4766-a462-343536653538/3st_van.svg"
+                                        alt=""
+                                    />
+                                    </div>
+                                </div>
+                                <div className="services__textwrapper t-cell t-valign_top">
+                                    <div
+                                    className="services__name t-name t-name_lg"
+                                    style={{ fontSize: 20 }}
+                                    >
+                                    <div style={{ fontSize: 20, fontFamily: '"Open Sans"' }}>
+                                        <strong>Поставка оборудования</strong>
+                                    </div>
+                                    </div>
+                                    <div
+                                    className="services__descr t-descr t-descr_xs"
+                                    style={{ fontSize: 20 }}
+                                    >
+                                        <div style={{ fontSize: 20, fontFamily: '"Open Sans"' }}>
+                                            Благодаря проверенным производителям оборудования и продуманным
+                                            техническим решениям, Вы можете быть уверены в надежности и
+                                            гарантированной бесперебойной работе системы при любых погодных
+                                            условиях.
+                                            <br />
+                                            <br />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li
+                            className="services__col t-list__item"
+                            style={{ marginTop: 30, transitionDelay: "0.48s" }}
+                        >
+                            <div className="services__item d-flex ">
+                                <div className="t-cell t-valign_top">
+                                    <div className="services__imgwrapper" style={{ width: 50 }}>
+                                    <img
+                                        className="services__img t-img"
+                                        src="https://static.tildacdn.com/lib/tildaicon/34663137-3763-4430-a237-376635336238/Tilda_Icons_38_Cleaning_company_repairer.svg"
+                                        alt=""
+                                    />
+                                    </div>
+                                </div>
+                                <div className="services__textwrapper t-cell t-valign_top">
+                                    <div
+                                    className="services__name t-name t-name_lg"
+                                    style={{ fontSize: 20 }}
+                                    >
+                                    <div style={{ fontSize: 20, fontFamily: '"Open Sans"' }}>
+                                        <strong>Ремонт и техническое обслуживание</strong>
+                                    </div>
+                                    </div>
+                                    <div
+                                    className="services__descr t-descr t-descr_xs"
+                                    style={{ fontSize: 20 }}
+                                    >
+                                    <div style={{ fontSize: 20, fontFamily: '"Open Sans"' }}>
+                                        При необходимости, готовы сделать реконструкцию или модернизацию
+                                        устаревшей вентиляционной системы. Предоставляем сервисное и
+                                        сезонное обслуживание смонтированных нами климатических систем.
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+ 
+export default ServicesMore;

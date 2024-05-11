7fc6e30ac12d320be741747c94f61c5cceae743b
Vlad Fedorov <79852880+Vlad19982602@users.noreply.github.com>
2024-05-11 19:05:31 +0500

    information about the services has been implemented

---
+++ src/components/how-work/HowWork.jsx
@@ -0,0 +1,223 @@
+import './howwork.css'
+
+const HowWork = () => {
+    return ( 
+        <>
+            <section className="howwork">
+                <div className="t-container">
+                    <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+                        <div className='howwork__title'><strong>КАК МЫ РАБОТАЕМ?</strong></div>
+                    </div>
+                    <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+                        <div className='howwork__subtitle'>Наш принцип - любовь к своему делу и честность в работе.</div>
+                    </div>
+                    <div className="howwork__item t-item">
+                        <div className="howwork__width100 howwork__width8 howwork__mainblock">
+                            <div
+                            className="howwork__col t-animate t-animate__chain_first-in-row t-animate_started t-animate__chain_showed"
+                            style={{ transitionDelay: "0.32s" }}
+                            >
+                            <div className="howwork__block">
+                                <div
+                                className="howwork__title howwork__name howwork__name-lg"
+                                >
+                                <div style={{ fontSize: 20 }} data-customstyle="yes">
+                                    <strong>Оставьте заявку</strong>
+                                </div>
+                                </div>
+                                <div
+                                className="howwork__desct howwork__text howwork__text-xs"
+                                >
+                                <div style={{ fontSize: 14 }} data-customstyle="yes">
+                                    Позвоните нам или отправьте заявку. Наши специалисты с радостью
+                                    проконсультируют Вас по интересующим вопросам.
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                            <div
+                            className="t-animate t-animate_started"
+                            style={{ transitionDelay: "0.32s" }}
+                            >
+                            <div className="howwork__line" style={{ backgroundColor: "#485358" }} />
+                            <div
+                                className="howwork__circle"
+                                style={{ backgroundColor: "#485358", border: "2px solid #485358" }}
+                            />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="howwork__item t-item">
+                        <div className="howwork__width100 howwork__width8 howwork__mainblock howwork__flex">
+                            <div
+                            className="howwork__col t547__flipped t-animate t-animate__chain_first-in-row t-animate_started t-animate__chain_showed"
+                            style={{ transitionDelay: "0.48s" }}
+                            >
+                            <div className="howwork__left">
+                                <div
+                                className="howwork__title howwork__name howwork__name-lg"
+                                >
+                                <strong>Обследование</strong>
+                                </div>
+                                <div
+                                className="howwork__desct howwork__text howwork__text-xs"
+                                >
+                                Мы приедем на объект для сбора информации. Если у Вас есть план
+                                помещения, направляйте нам его на электронную почту.
+                                </div>
+                            </div>
+                            </div>
+                            <div
+                            className="t-animate t-animate_started"
+                            style={{ transitionDelay: "0.32s" }}
+                            >
+                            <div className="howwork__line" style={{ backgroundColor: "#485358" }} />
+                            <div
+                                className="howwork__circle"
+                                style={{ backgroundColor: "#485358", border: "2px solid #485358" }}
+                            />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="howwork__item t-item">
+                        <div className="howwork__width100 howwork__width8 howwork__mainblock">
+                            <div
+                            className="howwork__col t-animate t-animate__chain_first-in-row t-animate_started t-animate__chain_showed"
+                            style={{ transitionDelay: "0s" }}
+                            >
+                            <div className="howwork__block">
+                                <div
+                                className="howwork__title howwork__name howwork__name-lg"
+                                >
+                                <strong>Расчёт вентиляции</strong>
+                                </div>
+                                <div
+                                className="howwork__desct howwork__text howwork__text-xs"
+                                >
+                                Разработаем техническое решение или проект вентиляции. Составим
+                                коммерческое предложение. В нём Вы получите изображение системы на
+                                плане и спецификацию, с указанием стоимости <br />
+                                всех затрат.
+                                </div>
+                            </div>
+                            </div>
+                            <div
+                            className="t-animate t-animate_started"
+                            style={{ transitionDelay: "0.32s" }}
+                            >
+                            <div className="howwork__line" style={{ backgroundColor: "#485358" }} />
+                            <div
+                                className="howwork__circle"
+                                style={{ backgroundColor: "#485358", border: "2px solid #485358" }}
+                            />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="howwork__item t-item">
+                        <div className="howwork__width100 howwork__width8 howwork__mainblock howwork__flex">
+                            <div
+                            className="howwork__col t547__flipped t-animate t-animate__chain_first-in-row t-animate_started t-animate__chain_showed"
+                            style={{ transitionDelay: "0.16s" }}
+                            >
+                            <div className="howwork__left">
+                                <div
+                                className="howwork__title howwork__name howwork__name-lg"
+                                >
+                                <strong>Согласование</strong>
+                                </div>
+                                <div
+                                className="howwork__desct howwork__text howwork__text-xs"
+                                >
+                                Согласование и утверждение технического решения и стоимости. Вы
+                                выберете подходящий вариант. Мы подготовим все требуемые документы.
+                                </div>
+                            </div>
+                            </div>
+                            <div
+                            className="t-animate t-animate_started"
+                            style={{ transitionDelay: "0.32s" }}
+                            >
+                            <div className="howwork__line" style={{ backgroundColor: "#485358" }} />
+                            <div
+                                className="howwork__circle"
+                                style={{ backgroundColor: "#485358", border: "2px solid #485358" }}
+                            />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="howwork__item t-item">
+                        <div className="howwork__width100 howwork__width8 howwork__mainblock">
+                            <div
+                            className="howwork__col t-animate t-animate__chain_first-in-row t-animate_started t-animate__chain_showed"
+                            style={{ transitionDelay: "0.16s" }}
+                            >
+                            <div className="howwork__block">
+                                <div
+                                className="howwork__title howwork__name howwork__name-lg"
+                                >
+                                <strong>Выполнение</strong>
+                                </div>
+                                <div
+                                className="howwork__desct howwork__text howwork__text-xs"
+                                >
+                                Выполним все работы, произведем монтаж вентиляции. Быстро,
+                                качественно, <br />
+                                точно в срок.
+                                <br />
+                                <br />
+                                </div>
+                            </div>
+                            </div>
+                            <div
+                            className="t-animate t-animate_started"
+                            style={{ transitionDelay: "0.32s" }}
+                            >
+                            <div className="howwork__line" style={{ backgroundColor: "#485358" }} />
+                            <div
+                                className="howwork__circle"
+                                style={{ backgroundColor: "#485358", border: "2px solid #485358" }}
+                            />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="howwork__item t-item">
+                        <div className="howwork__width100 howwork__width8 howwork__mainblock howwork__flex">
+                            <div
+                            className="howwork__col t547__flipped t-animate t-animate__chain_first-in-row t-animate_started t-animate__chain_showed"
+                            style={{ transitionDelay: "0.32s" }}
+                            >
+                            <div className="howwork__left">
+                                <div
+                                className="howwork__title howwork__name howwork__name-lg"
+                                >
+                                <strong>Завершение работы</strong>
+                                </div>
+                                <div
+                                className="howwork__desct howwork__text howwork__text-xs"
+                                >
+                                Готово! Здоровый микроклимат в Вашем доме или производстве
+                                благоприятно отразится на людях, находящихся внутри.
+                                <br />
+                                Свежим воздухом возможно дышать даже в Челябинске!
+                                </div>
+                            </div>
+                            </div>
+                            <div
+                            className="t-animate t-animate_started"
+                            style={{ transitionDelay: "0.32s" }}
+                            >
+                            <div className="howwork__line" style={{ backgroundColor: "#485358" }} />
+                            <div
+                                className="howwork__circle"
+                                style={{ backgroundColor: "#485358", border: "2px solid #485358" }}
+                            />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+     );
+}
+ 
+export default HowWork;
